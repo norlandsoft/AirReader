2a3f77daa71231f0a0dae2fd2baeda422b32e34c
norlandsoft <11669556@qq.com>
2026-06-03 14:02:30 +0800

    feat: 新建 Spring Boot 启动类

---
+++ src/main/java/com/airfilereader/Application.java
@@ -0,0 +1,21 @@
+package com.airfilereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AirFileReader 启动类
+ *
+ * PDF 文档内容读取服务，基于 OpenDataLoader 将 PDF 转换为 Markdown。
+ * 对外提供 Docling 风格的 REST API。
+ *
+ * @author ChaiMingXu
+ * @since 2026-06-03
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
